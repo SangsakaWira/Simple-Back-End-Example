ba8a4b5cf411e302b581cf486e5f0ec830852043
Sangsaka Wira <sangsakawira@gmail.com>
2022-07-28 13:12:26 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/get-all-locations", (req, res) => {
+  res.send([{
+    nama: "Kintamani",
+    foto:
+      "https://images.pexels.com/photos/4239546/pexels-photo-4239546.jpeg?cs=srgb&dl=pexels-alleksana-4239546.jpg&fm=jpg",
+    provinsi: "Bali",
+    negara: "Indonesia"
+  },{
+    nama: "Lombok",
+    foto:
+      "https://images.pexels.com/photos/3857215/pexels-photo-3857215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    provinsi: "NTB",
+    negara: "Indonesia"
+  }]);
+});
+
+app.listen(3000, (err) => {
+  console.log("Server is running!");
+});
